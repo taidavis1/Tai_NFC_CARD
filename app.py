de0bab7353c9f70139807a6f7867c31269c2507d
Tais-MacBook-Pro <jacobpham82@gmail.com>
2021-07-09 08:12:04 -0700

    chasndsa

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+import sys
+
+import time
+
+from flask import Flask , render_template, jsonify, redirect, url_for
+
+app = Flask(__name__ , static_folder= 'static' , template_folder= 'templates')
+
+app.config['SECRET_KEY'] = os.urandom(10)
+
+@app.route('/')
+def Home():
+    
+    return "Hello"
+
+
+
+#####################################3
+
+if __name__ == '__main__':
+    
+    app.run(host='127.0.0.1', debug=True, port=5000)
