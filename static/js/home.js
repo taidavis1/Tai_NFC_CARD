404859cae3ac06dceed164fc08fc6aca8f714b16
unknown <jacobpham82@gmail.com>
2021-07-12 12:38:17 -0700

    changes

---
+++ static/js/home.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+
+    $("#facebook").click(function(){
+
+        window.open('https://www.facebook.com/profile.php?id=100009281947149');
+
+
+    })
+
+
+    $("#git").click(function(){
+
+        window.open('https://github.com/taidavis1');
+
+
+    })
+
+    $("#foxv_shop").click(function(){
+
+        window.open('https://foxvshop.herokuapp.com/');
+
+
+    })
+
+    $("#phone").click(function(){
+
+        $("#phone_number").text("(408)-315-1453");
+
+    });
+
+    $("#youtube").click(function(){
+
+        window.open('http://taidavis1.pythonanywhere.com/');
+
+    });
+
+    
+
+});
